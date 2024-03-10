a8498027c7e6a837b5562bd2dd460e17f03d75a6
Ali Ahmad Fahrezy <aliahmadfahrezy@gmail.com>
2024-03-10 21:56:06 +0700

    Week 4

--- lib/gallery.dart
+++ lib/gallery.dart
@@ -8,15 +8,17 @@ class Gallery extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Header(
-      GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
+      OrientationBuilder(
+        builder: (builder, orientation) => GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: orientation == Orientation.portrait ? 3 : 6),
+          itemCount: orientation == Orientation.portrait ? 9 : 18,
+          itemBuilder: (context, index) {
+            return Image(
+              image: NetworkImage("https://picsum.photos/id/$index/400"),
+            );
+          },
         ),
-        itemCount: 9,
-        itemBuilder: (context, index) {
-          return const Image(
-              image: NetworkImage("https://placekitten.com/400"));
-        },
       ),
     );
   }
